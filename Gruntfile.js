019e2a38998f10106d268a99b9c8dbe1f612bc5e
Trung Kien <kienbt@tmh-techlab.vn>
2017-07-20 06:57:41 +0000

    Kien Init

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+   grunt.initConfig({
+    }
+ }
+ });
+// grunt.loadNpmTasks('grunt-contrib-concat');
+// grunt.loadNpmTasks('grunt-contrib-cssmin');
+// grunt.loadNpmTasks('grunt-contrib-uglify');
+// grunt.registerTask('default', ['concat', 'cssmin', 'uglify']);
+};
